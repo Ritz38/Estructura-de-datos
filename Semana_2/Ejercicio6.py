7f09796059b90dd5e1c2019aa5da5cc4508036b1
Juan Pablo Zuluaga Mesa <juanpablozuluagamesa@gmail.com>
2024-02-18 15:31:45 -0500

    Semana_2

--- Semana_2/Ejercicio6.py
+++ Semana_2/Ejercicio6.py
@@ -2,5 +2,14 @@ C = int(input())
 for i in range(C):
     T=int(input())
     J=tuple(map(int,input().split()))
-    
-        
+    i=0
+    c=()
+    while True:
+        if i>len(J)-1 or i<0:
+            break
+        c+=(i,)
+        if c.count(c[-1])==2:
+            c=c[:len(c)-1:]
+            break
+        i=i+J[i]
+    print(len(c))
