b4af62ea710b5bc66fc6bb87f67f39c6310b9014
Juan Pablo Zuluaga Mesa <juanpablozuluagamesa@gmail.com>
2024-08-06 15:47:48 -0500

    Create Ejercicio3.py

---
+++ Semana_7/Ejercicio3.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.contador = 0
+
+    def insert(self, key):
+        self.root = self._insertRecursively(self.root, key)
+
+    def _insertRecursively(self, root, key):
+        if root is None:
+            return Node(key)
+        if key < root.key:
+            root.left = self._insertRecursively(root.left, key)
+        elif key > root.key:
+            root.right = self._insertRecursively(root.right, key)
+        return root
+
+    def search(self, key):
+        if self._searchRecursively(self.root, key) != None:
+            return True
+        else:
+            return False
+
+    def _searchRecursively(self, root, key):
+        if root is None or root.key == key:
+            return root
+        if key < root.key:
+            return self._searchRecursively(root.left, key)
+        return self._searchRecursively(root.right, key)
+
+    def delete(self, key):
+        self.root = self._deleteRecursively(self.root, key)
+
+    def _deleteRecursively(self, root, key):
+        if root is None:
+            return root
+        if key < root.key:
+            root.left = self._deleteRecursively(root.left, key)
+        elif key > root.key:
+            root.right = self._deleteRecursively(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            root.key = self._minValueNode(root.right).key
+            root.right = self._deleteRecursively(root.right, root.key)
+        return root
+
+    def _minValueNode(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
+    def inOrder(self):
+        self._inOrderRecursively(self.root, self.contador)
+        return self.contador
+
+    def _inOrderRecursively(self, root, contador):
+        if root:
+            self._inOrderRecursively(root.left, contador)
+            if root.left == None and root.right == None: self.contador+=1
+            self._inOrderRecursively(root.right, contador)
+
+
+for _ in range(int(input())):
+    a = list(map(int,input().split()))
+    a = a[:-1:]
+    arbol = BinarySearchTree()
+    for i in a:
+        arbol.insert(i)
+    print(arbol.inOrder())
+
